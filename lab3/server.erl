3d732b596fdc15598b0a9f70f3dff4520ab736ef
Florian Minges <florianminges@gmail.com>
2014-02-26 13:41:09 +0100

    Clean up

--- lab3/server.erl
+++ lab3/server.erl
@@ -10,7 +10,6 @@ loop(St, {connect, Pid, Nick}) ->
             NewClients = St#server_st.clients ++ [Pid],    
             NewNicknames = St#server_st.nicknames ++ [Nick],
             NewState = St#server_st{clients=NewClients, nicknames=NewNicknames},
-            %io:fwrite("connected: ~w ", [NewClients]),
             {ok, NewState};
         true -> 
             {'EXIT', {error, nick_taken, "Nick already taken on server"}}
@@ -18,20 +17,18 @@ loop(St, {connect, Pid, Nick}) ->
     
     
 loop(St, {disconnect, Pid, Nick}) ->
-    NewClients = lists:delete(Pid, St#server_st.clients),
-    NewNicknames = lists:delete(Nick, St#server_st.nicknames),
-    %io:fwrite("disconnected: ~w ", [NewClients]),
-    NewState = St#server_st{clients=NewClients, nicknames=NewNicknames},
-    % TODO remove connection from chatrooms, if any left
+    NewClientList = lists:delete(Pid, St#server_st.clients),
+    NewNicknameList = lists:delete(Nick, St#server_st.nicknames),
+    NewState = St#server_st{clients=NewClientList, nicknames=NewNicknameList},
     {ok, NewState};
 
 loop(St, {join, _Channel, Pid}) ->
 	KeyFound = lists:member(_Channel, St#server_st.channels),
 	if	
-		false == KeyFound ->	
-			NewChannels = St#server_st.channels ++ [_Channel],
-			NewState = St#server_st{channels=NewChannels},
-			newChannel(_Channel),
+		KeyFound == false ->	
+			NewChannelList = St#server_st.channels ++ [_Channel],
+			NewState = St#server_st{channels=NewChannelList},
+			start_channel_process(_Channel),
 			genserver:request(list_to_atom(_Channel), {connect, Pid}),
 			{ok, NewState};
 		true ->			
@@ -39,8 +36,7 @@ loop(St, {join, _Channel, Pid}) ->
 			{ok, St}	
 	end.
 
-newChannel(_Channel) ->
-    %catch(unregister(list_to_atom(_Channel))),
+start_channel_process(_Channel) ->
     genserver:start(list_to_atom(_Channel), channel:initial_state(_Channel), 
                     fun channel:loop/2).
 

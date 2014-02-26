9f4b90d646216b6ec7b785c5aa9f4c1ed70791b3
Jakob <jakob@jakob-HP-Pavilion-dm1-Notebook-PC.(none)>
2014-02-26 13:00:55 +0100

    Comments and shit

--- lab3/channel.erl
+++ lab3/channel.erl
@@ -3,23 +3,32 @@
 
 -include_lib("./defs.hrl").
 
+%%%%%%%%%%%%%%%
+%%%% Connect
+%%%%%%%%%%%%%%%
 loop(St, {connect, Pid}) ->
     NewClients = St#channel_st.clients ++ [Pid],
     %io:fwrite("connected to channel: ~w ", [NewClients]),
     NewState = St#channel_st{clients=NewClients},
     {ok, NewState};
-    
+
+%%%%%%%%%%%%%%%
+%%%% Disconnect
+%%%%%%%%%%%%%%%    
 loop(St, {disconnect, Pid}) ->
     NewClients = lists:delete(Pid, St#channel_st.clients),
     %io:fwrite("disconnected to channel: ~w ", [NewClients]),
     NewState = St#channel_st{clients=NewClients},
     {ok, NewState};
 
+%%%%%%%%%%%%%%%
+%%%% Send message
+%%%%%%%%%%%%%%%
 loop(St, {msg_from_client, Pid, Name, _Msg}) ->
-	spawn(fun() -> newProcess(St, Pid, Name, _Msg) end),
+	spawn(fun() -> new_process(St, Pid, Name, _Msg) end),
 	{ok, St}.
 
-newProcess(St, Pid, Name, _Msg) ->
+new_process(St, Pid, Name, _Msg) ->
 	SendToList = lists:delete(Pid, St#channel_st.clients),
 		send_msg_to_client(SendToList, St#channel_st.name, Name, _Msg).
 
